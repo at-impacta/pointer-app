5d0f6de1a8f9e78cae20349b947b13adc80545fd
at-impacta <atus.silva@aluno.faculdadeimpacta.com.br>
2022-05-02 21:32:53 -0300

    Fix all files

--- dependencias.sh
+++ dependencias.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo yum update -y
+sudo apt-get update
+sudo apt-get install git ansible  -y
+
+git clone https://github.com/at-impacta/pointer-app.git /tmp/pointer-app/
+
+cd /tmp/pointer-app
+ansible-playbook pointer-playbook.yml
+
